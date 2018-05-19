b3de7e24e36e071d244043c65990d322cae3b6cc
Prangyy <pr.3ung@gmail.com>
2018-05-19 15:55:43 +0700

    Add logic for bot state

--- src/main/java/com/ecorp/linebot/LineBotApplication.java
+++ src/main/java/com/ecorp/linebot/LineBotApplication.java
@@ -1,5 +1,7 @@
 package com.ecorp.linebot;
 
+import com.ecorp.linebot.config.BotState;
+import com.ecorp.linebot.config.RegisterState;
 import com.ecorp.linebot.service.MessageService;
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
@@ -11,6 +13,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @SpringBootApplication
 @LineMessageHandler
 public class LineBotApplication {
@@ -18,7 +25,19 @@ public class LineBotApplication {
 	@Autowired
 	private MessageService messageService;
 
+	private BotState botState = BotState.Default;
+
+	private RegisterState registerState = RegisterState.None;
+
+	private static Map<String, Double> priceMap = new HashMap<String,Double>();
+
 	public static void main(String[] args) {
+
+		priceMap.put("ไข่ไก่", 3.50);
+		priceMap.put("ไข่เป็ด", 10.25);
+		priceMap.put("ไข่นกกะทา", 25.00);
+		priceMap.put("ไข่ห่าน", 3.22);
+
 		SpringApplication.run(LineBotApplication.class, args);
 	}
 
@@ -27,14 +46,63 @@ public class LineBotApplication {
 		System.out.println("event: " + event);
 
 		String gotMsg = event.getMessage().getText();
-		String sentMsg;
-		if(gotMsg.substring(0,1).equals("@")){
-			sentMsg = gotMsg.substring(1);
-			// Condition to check price of which coin and change path to query price
-			sentMsg = messageService.calculateData(sentMsg);
-		}
-		else {
-			sentMsg = event.getMessage().getText();
+		String sentMsg = "";
+		switch (botState) {
+			case Register:
+
+				if(gotMsg.equals("@cancel")){
+					botState = BotState.Default;
+					registerState = RegisterState.None;
+					sentMsg = "ยกเลิกการลงทะเบียนแล้วค่ะ";
+				}
+				else{
+					switch (registerState){
+						case WaitName:
+							registerState = RegisterState.WaitBirthDate;
+							sentMsg = "โปรดกรอกวันเกิดด้วยค่ะ";
+							break;
+						case WaitBirthDate:
+							registerState = RegisterState.WaitPhoneNumber;
+							sentMsg = "โปรดกรอกวันเกิดด้วยค่ะ";
+							break;
+						case WaitPhoneNumber:
+							botState = BotState.Default;
+							registerState = RegisterState.None;
+							sentMsg = "ลงทะเบียนสำเร็จ ขอบคุณค่ะ";
+							break;
+					}
+				}
+
+				break;
+
+			default:
+				if (gotMsg.equals("@register")){
+					botState = BotState.Register;
+					registerState = RegisterState.WaitName;
+					sentMsg = "ยินดีต้อนรับเข้าสู่บริการลงทะเบียน ถ้าต้องการยกเลิกการลงทะเบียนโปรดพิมพ์ @cancel\nโปรดกรอกชื่อด้วยค่ะ";
+				} else {
+
+					botState = BotState.Default;
+					registerState = RegisterState.None;
+
+					List<String> sentPrice = new ArrayList<String>();
+
+					for ( String key : priceMap.keySet() ) {
+						if(key.contains(gotMsg)){
+							
+						}
+					}
+
+					Double price = priceMap.get(gotMsg);
+					if(price == null){
+						sentMsg = "ขออภัยค่ะ ไม่สามารถประมวลผลได้";
+					}
+					else{
+						sentMsg = price.toString();
+					}
+				}
+
+				break;
 		}
 		return new TextMessage(sentMsg);
 	}
---
+++ src/main/java/com/ecorp/linebot/config/RegisterState.java
@@ -0,0 +1,11 @@
+package com.ecorp.linebot.config;
+
+/**
+ * Created by prangyy on 19/5/2018 AD.
+ */
+public enum RegisterState {
+    WaitName,
+    WaitBirthDate,
+    WaitPhoneNumber,
+    None
+}
